a2186f1e3e80c38c81a07211317af7eed031bd50
jeremiassflores <jeremiassflores@gmail.com>
2025-09-11 22:30:10 -0300

    Ya se creo el TP6
    falta mostrar en la tabla los productos que se agregan
    
    Co-Authored-By: Agustin Mazza <agus-mazza@hotmail.com>
    Co-Authored-By: Mauricio Damián Reta <mauricio.reta.03@gmail.com>

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+
+import java.util.ArrayList;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Jeremias
+ */
+public class Main {
+    //
+    
+    public static void main(String[] args) {
+        
+        
+        /*
+        Productos p11 = new Productos ("electronica", "Heladera" , 750);
+        Productos p12 = new Productos ("electronica", "Lavarropas" , 999);
+        Productos p13 = new Productos ("electronica", "Televesor" , 500);
+        
+        Productos p22 = new Productos ("perfumeria", "PacoRabanne" , 1500);
+        Productos p21 = new Productos ("perfumeria", "CH" , 2000);
+        Productos p23 = new Productos ("perfumeria", "CK" , 1000);
+        
+        Productos p31 = new Productos ("ropa", "Remera" , 99);
+        Productos p32 = new Productos ("ropa", "Pantalon" , 150);
+        Productos p33 = new Productos ("ropa", "Medias" , 10);
+        */
+        
+        
+        
+    }
+    
+}
